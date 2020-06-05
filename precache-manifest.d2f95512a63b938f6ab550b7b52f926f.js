5ff91a375ca624af1049de7452f4b24fb0f6e66e
Vincze János <vinczit@gmail.com>
2020-06-05 17:44:22 +0200

    Updates

---
+++ precache-manifest.d2f95512a63b938f6ab550b7b52f926f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b389050a5c0242a4fc75b456a47a478e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "6330c6e7f9cf78442427",
+    "url": "/pomodoro-clock/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "35cfad4d3dc24e58905a",
+    "url": "/pomodoro-clock/static/css/main.3403380a.chunk.css"
+  },
+  {
+    "revision": "6330c6e7f9cf78442427",
+    "url": "/pomodoro-clock/static/js/2.237a665a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.237a665a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cfad4d3dc24e58905a",
+    "url": "/pomodoro-clock/static/js/main.07983549.chunk.js"
+  },
+  {
+    "revision": "fb0cbabf91814b4c8401",
+    "url": "/pomodoro-clock/static/js/runtime-main.fc7fd16e.js"
+  }
+]);
